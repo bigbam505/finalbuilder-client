b21acab6e426f8e25ab51403a34479b97e79f42d
bigbam505 <brent@bmontague.com>
2013-09-17 00:43:01 +0530

    Remove uglify from grunt
    Closes #3

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,4 @@
 module.exports = function(grunt) {
 
-grunt.initConfig({
-  pkg: grunt.file.readJSON('package.json'),
-  uglify: {
-    options: {
-      banner: '/*! <%= pkg.name %> V<%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-    },
-    build: {
-      src: 'src/<%= pkg.name %>.js',
-      dest: 'lib/<%= pkg.name %>.min.js'
-    }
-  }
-});
-
-grunt.loadNpmTasks('grunt-contrib-uglify');
 grunt.loadNpmTasks('grunt-release');
-
 };
